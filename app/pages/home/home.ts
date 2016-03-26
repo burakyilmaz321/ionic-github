ae217f4aedcfb9e8265c80f970fc224edf34a6ea
Burak Yılmaz <burakyilmaz321@gmail.com>
2016-03-26 21:44:33 +0200

    Adds GitHubService, changes home.html

--- app/pages/home/home.ts
+++ app/pages/home/home.ts
@@ -1,6 +1,27 @@
 import {Page} from 'ionic-angular';
+import { GitHubService } from '../../services/git-hub-service';
 
 @Page({
-  templateUrl: 'build/pages/home/home.html'
+  templateUrl: 'build/pages/home/home.html',
+  providers: [GitHubService]
 })
-export class HomePage {}
+export class HomePage {
+    public foundRepos;
+    public username;
+    
+    constructor(private github: GitHubService) {
+    }
+    
+    getRepos() {
+        this.github.getRepos(this.username)
+            .subscribe(
+                data => {
+                    this.foundRepos = data.json();
+                },
+                
+                err => console.log(err),
+                
+                () => console.log('getRepos completed')
+            );
+    }
+}
